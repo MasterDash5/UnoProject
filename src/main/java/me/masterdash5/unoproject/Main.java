bb290027362e5d9b9318544599e73deb828f14b2
Xenny76 <dredford@student.neumont.edu>
2025-01-21 16:14:34 -0700

    Changed Main to a javaFX main application file

--- src/main/java/me/masterdash5/unoproject/Main.java
+++ src/main/java/me/masterdash5/unoproject/Main.java
@@ -1,9 +1,20 @@
 package me.masterdash5.unoproject;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("hello world");
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Uno.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Uno");
+        stage.setScene(scene);
+        stage.show();
     }
 
+    public static void main(String[] args) {
+        launch();
+    }
 }
