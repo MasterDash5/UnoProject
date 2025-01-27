a84cc843ab9a7c663716fc0e19b35b249b75aff3
Xenny76 <dredford@student.neumont.edu>
2025-01-27 15:33:04 -0700

    Resetting force draw to 0 when stacking draw2

--- src/main/java/me/masterdash5/unoproject/controller/UnoController.java
+++ src/main/java/me/masterdash5/unoproject/controller/UnoController.java
@@ -97,6 +97,7 @@ public class UnoController {
                 swapPlayers();
                 return true; // Wait for color selection to continue
             case DRAW2:
+                players[activePlayer].setForceDraw(0);
                 players[getNextPlayer()].setForceDraw(players[getNextPlayer()].getForceDraw() + 2);
                 break;
             case REVERSE:
